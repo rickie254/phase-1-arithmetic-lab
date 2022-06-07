4cfa95bb7e6590b40c407776600c4d6c812d0b9f
rickie maweu <rickiemaweu@gmail.com>
2022-06-08 02:10:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// Write your code here// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random()) + 1;
+
+const num3= 16;
+const num4= 6;
+
+const mod = num3%num4;
+
+const max=Math.max(2,4,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
